6cd5d848c1cafd1d8fa918b9bea1c4092b8fb4c7
Heloise <HeloiseS@users.noreply.github.com>
2024-02-09 11:00:32 +0000

    Started commenting  views.py

--- psat_server_web/atlas/atlasapi/views.py
+++ psat_server_web/atlas/atlasapi/views.py
@@ -1,3 +1,23 @@
+"""
+Author: Ken Smith
+Last Updated: 2024-02-09
+
+Notes for N00bs
+---------------
+* Why are there two authentication classes in the ConeView class?
+in a Django REST Framework view the authentication_classes specify which clases should be used to tdo the authenticification process. 
+Both options will be used in turn. First the TokenAuthentification class (which is a default in Django) and if that doesn't work it will try the 
+QueryAuthentication class which is a custom class that is defined in the query_auth module in this package. 
+
+* HTTP request methods:
+GET: retrieve data from the server .
+POST: submit data to be processed by the server.
+PUT: Update an existing resource on the server
+PATCH: Partially update an existing resource on the server (??)
+DELETE: Delete an existing resource on the server. 
+
+"""
+
 from django.shortcuts import get_object_or_404, render
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -8,6 +28,9 @@ from rest_framework.permissions import IsAuthenticated
 from .query_auth import QueryAuthentication
 
 def retcode(message):
+    """
+    Return error code based on message. 
+    """
     if 'error' in message: return status.HTTP_400_BAD_REQUEST
     else:                  return status.HTTP_200_OK
 
@@ -15,6 +38,7 @@ class ConeView(APIView):
     authentication_classes = [TokenAuthentication, QueryAuthentication]
     permission_classes = [IsAuthenticated]
 
+    # Handler for GET requests (one of the standard HTTP request methods)
     def get(self, request):
         serializer = ConeSerializer(data=request.GET, context={'request': request})
         if serializer.is_valid():
@@ -22,6 +46,7 @@ class ConeView(APIView):
             return Response(message, status=retcode(message))
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    # Handle for the POST requests (another one of the standard HTTP request metods)
     def post(self, request, format=None):
         serializer = ConeSerializer(data=request.data, context={'request': request})
         if serializer.is_valid():
